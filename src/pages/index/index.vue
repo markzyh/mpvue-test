f5131af995601bb08eceee06fc6388abadbb1fbc
markzyh <980511383@qq.com>
2018-11-14 22:43:57 +0800

    mpvue swiper is done

---
+++ src/pages/index/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container">
+    <bannerSwiper :imgUrls="imgUrls" />  
+  </div>
+</template>
+
+<script>
+import bannerSwiper from '@/components/swiper/swiper'
+
+export default {
+  data () {
+    return {
+      motto: 'Hello World',
+      userInfo: {},
+      imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ]
+    }
+  },
+
+  components: {
+   bannerSwiper
+  },
+
+  methods: {
+    bindViewTap () {
+      const url = '../logs/main'
+      wx.navigateTo({ url })
+    },
+    getUserInfo () {
+      // 调用登录接口
+      wx.login({
+        success: () => {
+          wx.getUserInfo({
+            success: (res) => {
+              this.userInfo = res.userInfo
+            }
+          })
+        }
+      })
+    },
+    clickHandle (msg, ev) {
+      console.log('clickHandle:', msg, ev)
+    }
+  },
+
+  created () {
+    // 调用应用实例的方法获取全局数据
+    this.getUserInfo()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
